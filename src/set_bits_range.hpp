373790c260a178421e20e2fb5b3938893a12f13e
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2020-03-29 10:21:17 -0700

    Add range over set bits type

---
+++ src/set_bits_range.hpp
@@ -0,0 +1,55 @@
+/* 
+ * File:   set_bits_range.h
+ * Author: jbosboom
+ *
+ * Created on March 28, 2020, 6:02 PM
+ */
+
+#ifndef SET_BITS_RANGE_H
+#define SET_BITS_RANGE_H
+
+#include <bit>
+
+struct set_bits_range_sentinel {};
+
+template<class Integer>
+struct set_bits_range_iterator {
+	Integer x;
+	unsigned int idx = 0;
+	set_bits_range_iterator(Integer x) : x(x) {
+		this->operator++();
+	}
+	set_bits_range_iterator& operator++() {
+		idx = std::countr_zero(x);
+		x &= ~(1 << idx);
+		return *this;
+	}
+	unsigned int operator*() const {
+		return idx;
+	}
+	bool operator==(set_bits_range_sentinel) const {
+		return !x;
+	}
+	bool operator!=(set_bits_range_sentinel) const {
+		return x;
+	}
+};
+
+template<class T>
+struct set_bits_range {
+	T t;
+	template<class X>
+	set_bits_range(X x) : t(+x) {} //unary plus to promote to uint
+
+	auto begin() const {
+		return set_bits_range_iterator<T>(t);
+	}
+	set_bits_range_sentinel end() const {
+		return {};
+	}
+};
+
+set_bits_range(unsigned int) -> set_bits_range<unsigned int>;
+
+#endif /* SET_BITS_RANGE_H */
+
