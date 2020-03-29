938bcae21134502c1eca27fa5601d17f545c0bc3
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2020-03-29 15:05:26 -0700

    Implement pushing

--- src/state.cpp
+++ src/state.cpp
@@ -15,11 +15,12 @@ struct State {
 };
 
 struct SharedWorkspace {
-	SharedWorkspace(const Board& b) {
+	SharedWorkspace(const Board& b) : board(b) {
 		neighbor_masks.reserve(b.squares());
 		for (unsigned int s = 0; s < b.squares(); ++s)
 			neighbor_masks.push_back(b.neighbors_mask(s));
 	}
+	const Board& board;
 	std::vector<uint32_t> neighbor_masks;
 
 	//TODO these should be in a Game object for non-Board rules customization
@@ -27,8 +28,89 @@ struct SharedWorkspace {
 	unsigned int allowable_moves_mask = 0b111; //0, 1 and 2 are all fine
 };
 
-void do_all_pushes(State source, const SharedWorkspace& work) {
-	//TODO implement pushing
+struct ThreadWorkspace {
+	std::vector<unsigned int> chain;
+};
+
+char remove_piece(State& state, unsigned int index) {
+	if (state.allied_pushers & (1 << index)) {
+		state.allied_pushers &= ~(1 << index);
+		return 'A';
+	} else if (state.allied_pawns & (1 << index)) {
+		state.allied_pawns &= ~(1 << index);
+		return 'a';
+	} else if (state.enemy_pushers & (1 << index)) {
+		state.enemy_pushers &= ~(1 << index);
+		return 'E';
+	} else if (state.enemy_pawns & (1 << index)) {
+		state.enemy_pawns &= ~(1 << index);
+		return 'e';
+	} else
+		throw std::logic_error("remove_piece: piece not present in any mask?");
+
+}
+
+void move_piece(State& state, unsigned int from, unsigned int to) {
+	//TODO make this branchless by extracting one bit, clearing it, and storing it back
+	if (state.allied_pushers & (1 << from)) {
+		state.allied_pushers &= ~(1 << from);
+		state.allied_pushers |= (1 << to);
+	} else if (state.allied_pawns & (1 << from)) {
+		state.allied_pawns &= ~(1 << from);
+		state.allied_pawns |= (1 << to);
+	} else if (state.enemy_pushers & (1 << from)) {
+		state.enemy_pushers &= ~(1 << from);
+		state.enemy_pushers |= (1 << to);
+	} else if (state.enemy_pawns & (1 << from)) {
+		state.enemy_pawns &= ~(1 << from);
+		state.enemy_pawns |= (1 << to);
+	} else
+		throw std::logic_error("move_piece: piece not present in any mask?");
+}
+
+void do_all_pushes(const State source, const SharedWorkspace& swork, ThreadWorkspace& twork) {
+	for (unsigned int start : set_bits_range(source.allied_pushers)) {
+		for (Dir dir : {LEFT, UP, RIGHT, DOWN}) {
+			twork.chain.clear();
+			twork.chain.push_back(start);
+
+			while (true) {
+				unsigned int next = swork.board.neighbor(twork.chain.back(), dir);
+				if (next == RAIL || source.anchored_pieces & (1 << next)) {
+					//TODO: if we have a torus, do we allow a circular push?
+					twork.chain.clear();
+					break; //push not possible
+				}
+				if (next == VOID) {
+					twork.chain.push_back(next);
+					break;
+				}
+				if (source.blockers() & (1 << next))
+					twork.chain.push_back(next);
+				else
+					break;
+			}
+			if (twork.chain.size() < 2)
+				continue;
+
+			State next = source;
+			char removed_piece = ' ';
+			if (twork.chain.back() == VOID) {
+				if (twork.chain.size() == 2)
+					continue; //can't push nothing into the void
+				twork.chain.pop_back(); //pops void, leaves removed piece at back()
+				removed_piece = remove_piece(next, twork.chain.back());
+			}
+
+			assert(twork.chain.size() >= 2);
+			for (auto i = twork.chain.size()-1; i-- > 0;)
+				move_piece(next, twork.chain[i], twork.chain[i+1]);
+			//TODO: if we have multiple anchored pieces, how do we update?
+			next.anchored_pieces = 1u << twork.chain[1]; //anchor where the pusher moved to
+
+			//visitor.visit(next, removed_piece);
+		}
+	}
 }
 
 uint32_t connected_empty_space(unsigned int source, uint32_t blockers, const SharedWorkspace& work) {
@@ -46,31 +128,36 @@ uint32_t connected_empty_space(unsigned int source, uint32_t blockers, const Sha
 	return result;
 }
 
-void next_states(State source, unsigned int move_number, const SharedWorkspace& work) {
-	if (work.allowable_moves_mask & (1 << move_number))
-		do_all_pushes(source, work);
+void next_states(const State source, unsigned int move_number, const SharedWorkspace& swork, ThreadWorkspace& twork) {
+	//to allow detecting positions with no moves we need a begin/end pair on the acceptor.
+	//if (move_number == 0)
+	//	visitor.begin(source);
+	if (swork.allowable_moves_mask & (1 << move_number))
+		do_all_pushes(source, swork, twork);
 
-	if (move_number < work.max_moves) {
+	if (move_number < swork.max_moves) {
 		//Make a move.
 		for (unsigned int from : set_bits_range(source.allied_pushers)) {
-			uint32_t all_to = connected_empty_space(from, source.blockers(), work);
+			uint32_t all_to = connected_empty_space(from, source.blockers(), swork);
 			for (unsigned int to : set_bits_range(all_to)) {
 				State next = source;
 				next.allied_pushers &= ~(1 << from);
 				next.allied_pushers |= (1 << to);
-				next_states(source, move_number+1, work);
+				next_states(source, move_number+1, swork, twork);
 			}
 		}
 		for (unsigned int from : set_bits_range(source.allied_pawns)) {
-			uint32_t all_to = connected_empty_space(from, source.blockers(), work);
+			uint32_t all_to = connected_empty_space(from, source.blockers(), swork);
 			for (unsigned int to : set_bits_range(all_to)) {
 				State next = source;
 				next.allied_pawns &= ~(1 << from);
 				next.allied_pawns |= (1 << to);
-				next_states(source, move_number+1, work);
+				next_states(source, move_number+1, swork, twork);
 			}
 		}
 	}
+	//if (move_number == 0)
+	//	visitor.end(source);
 }
 
 } //namespace pushfight
